e266b29bc0d135adb18b6be9ad4d16b8a7d6b8f8
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-12-29 08:45:06 +0000

    Add logic on menu btn

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,11 +6,41 @@ interface NavProps {
 }
 
 interface NavState {
-
+    navTitle: string;
+    display: boolean;
+    navigation: string[];
 }
 
 export default class Nav extends React.Component<NavProps, NavState> {
+    constructor(props: string) {
+        super(props);
+        this.state = {
+            navTitle: 'Menu',
+            display: false,
+            navigation: [
+                'home',
+                'about',
+                'work',
+                'blog'
+            ]
+        }
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        const currentState = this.state.display;
+        this.setState({ display: !currentState });
+    }
+
     render() {
-        return <div></div>
+        const { navTitle, navigation } = this.state;
+        return <div onClick={this.toggleNav}>
+            <div>
+                {navTitle}
+            </div>
+            <nav>
+                {navigation.map(li=> `${li}  `)}
+            </nav>
+        </div>
     }
 }
