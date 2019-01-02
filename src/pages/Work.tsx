d5ff9415ae88e14d95d1510ef06156c3968f5bb6
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2019-01-02 02:43:41 +0100

    Add Work page

---
+++ src/pages/Work.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+interface WorkProps {
+
+}
+
+interface WorkState {
+
+}
+
+export default class Work extends React.Component {
+    render() {
+        return <div>Work</div>
+    }
+}
