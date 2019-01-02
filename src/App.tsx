b9be6c8f969b51cd091319117a471046cd31f7c8
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2019-01-02 02:42:33 +0100

    Modify App according to ReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import styled from '@emotion/styled';
+import Header from './components/Header';
 import Landing from './pages/Landing';
 import About from './pages/About';
+import Work from './pages/Work';
 
 const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr;
 `;
 
-interface AppProps {
-}
-interface AppState {
-}
-
-export default class App extends React.Component<AppProps, AppState> {
+export default class App extends React.Component<{}, {}> {
   render() {
-    return (
+    return <Router>
       <Container>
-        <Landing />
-        <About />
+        <Header />
+        <Route exact path="/" component={Landing}/>
+        <Route path="/home" component={Landing}/>
+        <Route path="/about" component={About}/>
+        <Route path="/work" component={Work}/>
       </Container>
-    );
+    </Router>
   }
 }
