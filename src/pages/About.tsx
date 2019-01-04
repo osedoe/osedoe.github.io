0d22aba65f5e5797a8484098ae9f02fbd5958104
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2019-01-04 18:06:42 +0000

    Add skills to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -50,6 +50,7 @@ interface AboutState {
     sectionTitle: string;
     sectionSubtitles: string[];
     sectionText: string[];
+    sectionSkills: string[];
 }
 
 export default class About extends React.Component<{}, AboutState> {
@@ -67,7 +68,8 @@ export default class About extends React.Component<{}, AboutState> {
                 'My name is Jose Diaz Gonzalez, and I am a Full Stack Web Developer based in Tenerife, Spain. In 2015, I moved to Bristol, UK. There I found my passion and started learning everything about Web Development In 2017, I moved back to Spain and got a Diploma in Development of Web Applications. I also went through the EOI\'s Full Stack Web Development Bootcamp.',
                 'I am truly passionate about the craft of programming and problem-solving, and love all things web. I specialize in the Front-End, ReactJS and NodeJS, but I can dive into Back-End, databases and DevOps easily. I am a JavaScript/TypeScript advocate but I have dabbled with Java SE/EE and PHP in the past.',
                 'When I\'m not building websites, going through CodeWars or promoting OSS -I like indie games, cooking, and playing pool. Oh, also I\'m a dog lover and the proud son of Suka, a half a staffy half dalmatian dog.'
-            ]
+            ],
+            sectionSkills: [ 'HTML/CSS', 'JavaScript', 'TypeScript', 'ReactJS', 'NodeJS', 'Java SE/EE', 'SASS/SCSS', 'MySQL', 'Git', 'Docker' ]
         }
     }
     
