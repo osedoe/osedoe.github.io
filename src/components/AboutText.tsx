7c669bd4e7d1010e726191e25f0b0b22d409a32d
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2019-01-04 20:27:49 +0000

    Refactor about divs into components

--- src/components/AboutText.tsx
+++ src/components/AboutText.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Div = styled.div`
+    display: grid;
+    grid-template-rows: 1fr 3fr;
+    margin: .3em;
+    padding: .3em;
+    height: 100%;
+`;
+
+const SubHeading = styled.h3`
+    color: var(--blue);
+    font-family: audiowide;
+    padding: .3em;
+    margin: .1em;
+`;
+
+const Paragraph = styled.p`
+    font-family: 'Exo 2';
+    padding: .5em 4em .5em .5em;
+    margin: .1em;
+    text-align: justify;
+`;
 
 interface AboutTextProps {
-
+    subtitle: string;
+    text: string;
 }
 
 interface AboutTextState {
 
 }
 
-export default class AboutText extends React.Component {
+export default class AboutText extends React.Component<AboutTextProps, AboutTextState> {
     render() {
-        return <div>
-            BLUBLU
-        </div>
+        const { subtitle, text } = this.props;
+        return <Div>
+        <SubHeading>{subtitle}</SubHeading>
+        <Paragraph>{text}</Paragraph>
+        </Div>
     }
 }
