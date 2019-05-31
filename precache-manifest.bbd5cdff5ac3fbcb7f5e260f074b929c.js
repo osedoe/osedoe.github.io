149a9f624c9e37a0ebc0483f33dc99e15092cac6
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2019-05-31 19:21:33 +0100

    Updates

---
+++ precache-manifest.bbd5cdff5ac3fbcb7f5e260f074b929c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dea9967980e7477a11e",
+    "url": "/static/css/main.137a22c7.chunk.css"
+  },
+  {
+    "revision": "0dea9967980e7477a11e",
+    "url": "/static/js/main.328aedd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6f56a2d5f094de004e75",
+    "url": "/static/js/2.38c2b6b8.chunk.js"
+  },
+  {
+    "revision": "dd208321d96a9b8952f7763744499e45",
+    "url": "/index.html"
+  }
+];
